75749aa850ebca9ea1aaa17ea66e97a6a4395ac5
letitia-chiu <hsuan.acc@outlook.com>
2023-10-19 14:11:23 +0800

    Build: add json file for url storage & establish base modle of redirection

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 const {engine} = require('express-handlebars')
+const urls = require('./public/jsons/url.json')
+
 
 app.use(express.static('public'))
 app.engine('.hbs', engine({extname: '.hbs'}))
@@ -11,6 +13,14 @@ app.set('views', './views')
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+app.get('/:id', (req, res) => {
+  const id = req.params.id
+  if (id === urls[0].id) {
+    res.redirect(urls[0].orig)
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`Express server is running on http://localhost:${port}`)
